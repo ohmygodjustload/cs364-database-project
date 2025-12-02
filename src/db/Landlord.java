2dd1bddf9967202a259b360303f89d23924d36e1
Andrew Peirce <drew101peirce@gmail.com>
2025-12-01 19:26:01 -0600

    Initial commit
    Signed-off-by: Andrew Peirce <drew101peirce@gmail.com>

---
+++ src/db/Landlord.java
@@ -0,0 +1,44 @@
+/**
+ * A POJO class representing a Landlord entity.
+ * @author Andrew Peirce
+ * Date Last Modified: December 1, 2025
+ */
+public class Landlord {
+    private int LLID;
+    private String name;
+    private String phoneNum;
+    private String email;
+
+    public Landlord(int LLID, String name, String phoneNum, String email) {
+        this.LLID = LLID;
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.email = email;
+    }
+
+    public int getLLID() {
+        return LLID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Landlord{" +
+                "LLID=" + LLID +
+                ", name='" + name + '\'' +
+                ", phoneNum='" + phoneNum + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
