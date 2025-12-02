b53950c812f1bd76b123cd4966e49c6ec2e40516
Andrew Peirce <drew101peirce@gmail.com>
2025-12-01 18:27:03 -0600

    Initial commit
    Signed-off-by: Andrew Peirce <drew101peirce@gmail.com>

---
+++ db/RelationshipGenerator.java
@@ -0,0 +1,11 @@
+/**
+ * This class is responsible for generating relationships between Tenants, Properties, and Landlords in the database.
+ * 
+ * @author Andrew Peirce
+ * Date Last Modified: December 1, 2025
+ */
+public class RelationshipGenerator {
+    // Phase 1: Generate LivesIn relationships
+
+    // Phase 2: Derive LeasesFrom relationships from LivesIn
+}
