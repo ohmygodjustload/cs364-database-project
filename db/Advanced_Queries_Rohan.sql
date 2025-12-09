9cb7ed8dd9d5ea299cd3694bb8b5f8c13c2e442f
randenath <randenath@gmail.com>
2025-12-09 16:49:48 -0600

    added second advanced query

--- db/Advanced_Queries_Rohan.sql
+++ db/Advanced_Queries_Rohan.sql
@@ -1,7 +1,22 @@
 -- Rohan's advanced queries
 
-SELECT FName, LName, Budget, Email, PID
-    FROM Tenant t JOIN LivesIn l ON t.SSN = l.SSN
-    WHERE Budget > 1000
-    ORDER BY Budget DESC
+-- GROUP 1: Find the number of tenants living in each property, but also show properties
+--          who's address contains "St", and sorted by tenant
+SELECT p.PID, p.Address, COUNT(l.SSN) AS NumTenants
+    FROM Property p JOIN LivesIn l ON p.PID = l.PID
+    WHERE p.Address LIKE '%St%'
+    GROUP BY p.PID, p.Address
+    ORDER BY NumTenants DESC
     LIMIT 10;
+
+-- GROUP 2: Find the first 20 landlords with properties that have more than 2 tenants.
+--          Skip the first 5 because they are hoarders probably.
+SELECT ll.LLID, ll.Name, COUNT(t.SSN) AS TotalTenants
+    FROM LandLord ll LEFT JOIN Property p ON ll.LLID = p.LLID
+        LEFT JOIN LivesIn li ON p.PID = li.PID
+        LEFT JOIN Tenant t ON li.SSN = t.SSN
+    GROUP BY ll.LLID, ll.Name
+    HAVING COUNT(t.SSN) > 2
+    ORDER BY TotalTenants DESC;
+    LIMIT 20
+    OFFSET 5;
