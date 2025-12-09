e5d1d11b0223a815ac9a9db0264c6a2f93fecf38
randenath <randenath@gmail.com>
2025-12-09 16:59:19 -0600

    Added my third advanced query

--- db/Advanced_Queries_Rohan.sql
+++ db/Advanced_Queries_Rohan.sql
@@ -12,7 +12,7 @@ SELECT p.PID, p.Address, COUNT(l.SSN) AS NumTenants
 -- GROUP 2: Find the first 20 landlords with properties that have more than 2 tenants.
 --          Skip the first 5 because they are hoarders probably.
 SELECT ll.LLID, ll.Name, COUNT(t.SSN) AS TotalTenants
-    FROM LandLord ll LEFT JOIN Property p ON ll.LLID = p.LLID
+    FROM Landlord ll LEFT JOIN Property p ON ll.LLID = p.LLID
         LEFT JOIN LivesIn li ON p.PID = li.PID
         LEFT JOIN Tenant t ON li.SSN = t.SSN
     GROUP BY ll.LLID, ll.Name
@@ -20,3 +20,11 @@ SELECT ll.LLID, ll.Name, COUNT(t.SSN) AS TotalTenants
     ORDER BY TotalTenants DESC;
     LIMIT 20
     OFFSET 5;
+
+-- GROUP 3: Find tenants who have a higher budget than the average budget of all the tenants in their properties
+SELECT t.FName, t.LName, t.Budget, li.PID
+    FROM Tenant t JOIN LivesIn li ON t.SSN = li.SSN
+    WHERE t.Budget > (SELECT AVG(t2.Budget)
+                            FROM LivesIn li2 JOIN Tenant t2 ON li2.SSN = t2.SSN
+                            WHERE li2.PID = li.PID)
+    ORDER BY t.Budget DESC;
